65969aa062a0442b212d11931cd2dd73ed67e016
anuradha.wijesinghe88 <anuradha.wijesinghe88@gmail.com>
2013-05-06 11:37:00 +0000

    all pages Initialized at pages's class

--- SalEx/src/salex/Pages.java
+++ SalEx/src/salex/Pages.java
@@ -26,12 +26,50 @@ public class Pages {
         COLLECTION_REPORT = loadTab("CollectionReportView", "Collection Report");
         COMPANY = loadTab("CompanyView", "Company Details");
         COST_PRICE_CHANGE = loadTab("CostPriceChangeView", "Cost Price Change");
-        DAMAGE_NOTES = loadTab("DamageNotesView", "Employee Details");
+        DAMAGE_NOTES = loadTab("DamageNotesView", "Damage Notes");
         DAMAGE_STOCK = loadTab("DamageStockView", "Damage Stock");
         EMPLOYEE_POSITION = loadTab("EmployeePositionView", "Employee Position");
         EMPLOYEE = loadTab("EmployeeView", "Employee Details");
         EXPENSES_PAY = loadTab("ExpensesPayView", "Expenses Pay");
         EXPENSES_PEPORT = loadTab("ExpensesReportView", "Expenses Report");
+        FIND_INVOICE = loadTab("FindInvoiceViewController", "Find Invoice");
+        HOME = loadTab("HomeView", "Home ");
+        ISSUED_CHEQUE_REPORT = loadTab("IssuedChequeReportView", "Issued Cheque Report ");
+        ITEM_MOVEMENT = loadTab("ItemMovementView", "Item Movement ");
+        ITEM_SALES_REPORT = loadTab("ItemSalesReportView", "Item Sales Report");
+        ITEM_TYPE = loadTab("ItemTypeView", "Item Type ");
+        ITEM = loadTab("ItemView", "Item ");
+        LOGIN = loadTab("Login", "Login ");
+        MAIN = loadTab("Main", "Main ");
+        MENU_MANAGE = loadTab("MenuManageView", "Menu Manage ");
+         MONTH_REPORT = loadTab("MonthReportView", "Month Report  ");
+        OPERATOR = loadTab("OperatorView", "Operator");
+        PART_PAYMENT_COLLECTION_REPORT = loadTab("PartPaymentCollectionReportView", "Part Payment Collection Report");
+        PAYMENT_REPORT = loadTab("PaymentReportView", "Payment Report");
+        PERMISSION = loadTab("PermissionView", "Permission ");
+        PROFIT_SUMMERY = loadTab("ProfitSummeryView", "Profit Summery");
+        PROFIT = loadTab("ProfitView", "Profit");
+        PURCHASE_INVOICE_PAYMENT = loadTab("PurchaseInvoicePaymentView", "Purchase Invoice Payment ");
+        PURCHASE_INVOICE = loadTab("PurchaseInvoiceView", "Purchase Invoice");
+        PURCHASE_REPORT = loadTab("PurchaseReportView", "Purchase Report");
+        RECEIVED_CHEQUE_REPORT = loadTab("ReceivedChequeReportView", "Received Cheque Report ");
+        REP_SALE_REPORT = loadTab("RepSaleReportView", "RepSale Report ");
+        ROOT_AREA = loadTab("RootAreaView", "Root Area");
+        SALE_INVOICE_PAYMENT = loadTab("SaleInvoicePaymentView", "Sale Invoice Payment");
+        SALE_REPORT = loadTab("SalesReport", "Sales Report ");
+        SELING_PRICE_CHANGE = loadTab("SelingPriceChangeView", "Seling Price Change");
+        STOCK_CHANGE_REPORT = loadTab("StockChangeReportView", "Stock Change Report");
+        STOCK_CHANGE = loadTab("StockChangeView", "Stock Change");
+        STOCK_TRANSFER = loadTab("StockTransferView", "Stock Transfer");
+        STOCK = loadTab("StockView", "Stock");
+        SUB_DEALER = loadTab("SubDealerView", "Sub Dealer");
+        SUPPLIER = loadTab("SupplierView", "Supplier");
+        TOWN = loadTab("TownView", "Town");
+        TRANSFER_REPORT = loadTab("TransferReportView", "Transfer Report");
+
+
+
+
     }
 
     public static Tab loadTab(String fxmlName, String tabTitle) {
