36d7d84015ef7ed0acc553a9926454cdaa879bbd
j.sandun.j <j.sandun.j@gmail.com>
2013-05-10 17:51:59 +0000

    @CostPriceChangeViewController named combobox as 'itemComboBox' Named colum fx ids coded for view item at comboBox
    @DamageNotesViewController
    named combobox as 'repComboBox' 
    Named colum fx ids
    coded for view item at comboBox
    
    @ItemMovementViewController
    named combobox as 'itemComboBox'
    Named colum fx ids
    coded for view item at comboBox
    
    @OperatorView
    Named colum fx ids
    coded for view item at comboBox
    
    @PurchaseInvoicePaymentView
    Named colum fx ids
    coded for view items at comboBoxes(supplier&bank)
    
    @RepSaleReportView
    Named colum fx ids
    coded for view item at comboBox
    
    @TransferReportView
    coded for view item at comboBox

--- SalEx/src/salex/controller/ItemMovementViewController.java
+++ SalEx/src/salex/controller/ItemMovementViewController.java
@@ -7,10 +7,15 @@ package salex.controller;
 import com.sai.javafx.calendar.FXCalendar;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
 import javafx.scene.layout.HBox;
 import salex.SuperController;
+import salex.ent.EmployeePosition;
+import salex.ent.Item;
 
 /**
  * FXML Controller class
@@ -21,6 +26,16 @@ public class ItemMovementViewController extends SuperController implements Initi
 
     @FXML
     private HBox dateHBox;
+    @FXML
+    private ComboBox<Item> itemcomboBox;
+    @FXML
+    private TableColumn<?, ?> receive;
+    @FXML
+    private TableColumn<?, ?> receiveQuantityTableColum;
+    @FXML
+    private TableColumn<?, ?> issueDateTableColum;
+    @FXML
+    private TableColumn<?, ?> issueQuantityTableColum;
 
     /**
      * Initializes the controller class.
@@ -29,5 +44,10 @@ public class ItemMovementViewController extends SuperController implements Initi
     public void initialize(URL url, ResourceBundle rb) {
         FXCalendar calendar = new FXCalendar();
         dateHBox.getChildren().add(calendar);
+        fillItemComboBox();
+    }
+
+    private void fillItemComboBox() {
+         itemcomboBox.setItems(FXCollections.observableList(manager.find(Item.class)));
     }
 }
--- SalEx/src/salex/controller/RepSaleReportViewController.java
+++ SalEx/src/salex/controller/RepSaleReportViewController.java
@@ -7,11 +7,15 @@ package salex.controller;
 import com.sai.javafx.calendar.FXCalendar;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
 import javafx.scene.layout.HBox;
 import salex.SuperController;
+import salex.ent.RepSale;
+import salex.ent.SaleCheque;
 
 /**
  * FXML Controller class
@@ -23,7 +27,17 @@ public class RepSaleReportViewController extends SuperController implements Init
     @FXML
     private HBox dateHbox;
     @FXML
-    private ComboBox<?> repComboBox;
+    private ComboBox<RepSale> repComboBox;
+    @FXML
+    private TableColumn<?, ?> codeTableColumn;
+    @FXML
+    private TableColumn<?, ?> descriptionTableColumn;
+    @FXML
+    private TableColumn<?, ?> loadedTableColumn;
+    @FXML
+    private TableColumn<?, ?> remainingTableColumn;
+    @FXML
+    private TableColumn<?, ?> soldTableColumn;
 
     /**
      * Initializes the controller class.
@@ -32,5 +46,11 @@ public class RepSaleReportViewController extends SuperController implements Init
     public void initialize(URL url, ResourceBundle rb) {
         FXCalendar calendar = new FXCalendar();
         dateHbox.getChildren().add(calendar);
+        fillRepCombmBox();
     }
+
+    private void fillRepCombmBox() {
+         repComboBox.setItems(FXCollections.observableList(manager.find(RepSale.class)));
+        
+         }
 }
