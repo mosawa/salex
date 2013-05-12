5c91927bd7e1d52d9c8aa98eec22f12db93da332
r.thilina <r.thilina@gmail.com>
2013-05-12 11:15:08 +0000

    @ ItemTypeView table and table column fxid added
    @ ItemTypeViewController
    fill and fill table functions implemented
    delete function implemented

--- SalEx/src/salex/controller/ItemTypeViewController.java
+++ SalEx/src/salex/controller/ItemTypeViewController.java
@@ -6,12 +6,19 @@ package salex.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.InputEvent;
 import salex.SuperController;
+import salex.ent.Bank;
+import salex.ent.ItemType;
 
 /**
  * FXML Controller class
@@ -28,16 +35,35 @@ public class ItemTypeViewController extends SuperController implements Initializ
     private Button deleteButton;
     @FXML
     private Button clearButton;
+    @FXML
+    private TableView<ItemType> itemTypeTablView;
+    @FXML
+    private TableColumn<ItemType, String> typeTableColumn;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        makeColumns();
+        fillTable();
+    }
+
+    private void fillTable() {
+        itemTypeTablView.setItems(FXCollections.observableList(manager.find(ItemType.class)));
+    }
+
+    private void makeColumns() {
+        typeTableColumn.setCellValueFactory(
+                new PropertyValueFactory<ItemType, String>("type"));
     }
 
     @FXML
+    private void fill(InputEvent event) {
+        ItemType itemType = itemTypeTablView.getSelectionModel().getSelectedItem();
+        typeTextield.setText(itemType.getType());
+    }
+
     private void gotoUpDateButton(ActionEvent event) {
         updateButton.requestFocus();
     }
@@ -49,11 +75,15 @@ public class ItemTypeViewController extends SuperController implements Initializ
 
     @FXML
     private void delete(ActionEvent event) {
+        manager.delete(itemTypeTablView.getSelectionModel().getSelectedItem());
+        clear(event);
     }
 
     @FXML
     private void clear(ActionEvent event) {
         typeTextield.setText("");
-        
+        fillTable();
+
     }
+    
 }
--- SalEx/src/salex/controller/ItemViewController.java
+++ SalEx/src/salex/controller/ItemViewController.java
@@ -4,7 +4,13 @@
  */
 package salex.controller;
 
+import com.nanosl.lib.log.Loggings;
+import java.io.Serializable;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -12,10 +18,19 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.InputEvent;
 import salex.SuperController;
+import salex.ent.Item;
 import salex.ent.ItemType;
+import salex.ent.LastCode;
+import salex.ent.PriceList;
+import salex.ent.Stock;
 import salex.ent.Supplier;
+import static util.Format.nf2d;
 
 /**
  * FXML Controller class
@@ -42,6 +57,18 @@ public class ItemViewController extends SuperController implements Initializable
     private Button deleteButton;
     @FXML
     private Button clearButton;
+    @FXML
+    private TableView<ItemTableItem> itemTable;
+    @FXML
+    private TableColumn<ItemTableItem, String> hashColumn;
+    @FXML
+    private TableColumn<ItemTableItem, String> codeColumn;
+    @FXML
+    private TableColumn<ItemTableItem, String> descriptionColumn;
+    @FXML
+    private TableColumn<ItemTableItem, String> quantityColumn;
+    @FXML
+    private TableColumn<ItemTableItem, String> priceColumn;
 
     /**
      * Initializes the controller class.
@@ -50,10 +77,27 @@ public class ItemViewController extends SuperController implements Initializable
     public void initialize(URL url, ResourceBundle rb) {
         fillSupplierComboBox();
         fillTypeComboBox();
+        makeColumns();
+        fillTable();
+    }
+
+    private void makeColumns() {
+        hashColumn.setCellValueFactory(
+                new PropertyValueFactory<ItemTableItem, String>("row"));
+        codeColumn.setCellValueFactory(
+                new PropertyValueFactory<ItemTableItem, String>("code"));
+        descriptionColumn.setCellValueFactory(
+                new PropertyValueFactory<ItemTableItem, String>("description"));
+        quantityColumn.setCellValueFactory(
+                new PropertyValueFactory<ItemTableItem, String>("quantity"));
+        priceColumn.setCellValueFactory(
+                new PropertyValueFactory<ItemTableItem, String>("amount"));
     }
 
     @FXML
     private void gotoDescription(ActionEvent event) {
+        String code = codeTextField.getText().trim();
+        fill(manager.find(Item.class, code));
         descriptionTextField.requestFocus();
     }
 
@@ -74,13 +118,83 @@ public class ItemViewController extends SuperController implements Initializable
 
     @FXML
     private void update(ActionEvent event) {
+        String code = codeTextField.getText().trim();
         String description = descriptionTextField.getText().trim();
         String brand = brandTextField.getText().trim();
-        supplierComboBox.getSelectionModel().getSelectedItem();
+        Supplier supplier = supplierComboBox.getSelectionModel().getSelectedItem();
+
+        ItemType itemType = typeComboBox.getSelectionModel().getSelectedItem();
+
+        if (code.equals("")) {
+            codeTextField.requestFocus();
+            return;
+        }
+        if (description.equals("")) {
+            descriptionTextField.requestFocus();
+            return;
+        }
+        if (supplier == null) {
+            showError("Supplier Not selected");
+            supplierComboBox.requestFocus();
+            supplierComboBox.getSelectionModel().clearAndSelect(0);
+        }
+
+        try {
+            String minLimText = minimunLimitTextField.getText().trim();
+            minLimText = minLimText.isEmpty() ? "0.0" : minLimText;
+            double minimumLimit = Double.parseDouble(minLimText);
+            Item item = manager.find(Item.class, code);
+            List<Serializable> serializables = new ArrayList<Serializable>();
+            if (item == null) {
+                item = new Item(code);
+                LastCode lastCode = manager.find(LastCode.class, "Item");
+                lastCode = lastCode == null ? new LastCode("Item") : lastCode;
+                lastCode.setCode(code);
+                serializables.add(lastCode);
+            }
+            item.setDescription(description);
+            item.setBrand(brand);
+            item.setSupplier(supplier);
+//            ItemType itemType = (ItemType) itemTypeComboBox.getSelectedItem();
+            item.setItemTypeType(itemType);
+            Stock stock = item.getStock();
+            if (stock == null) {
+                stock = new Stock(code);
+                stock.setBundles(0.0);
+                stock.setItem(item);
+                stock.setQuantity(0.0);
+                stock.setMinLimit(minimumLimit);
+                item.setStock(stock);
+                serializables.add(stock);
+            }
+            supplier.getItemCollection().add(item);
+            PriceList priceList = manager.find(PriceList.class, code);
+            priceList = priceList == null ? new PriceList(code) : priceList;
+            priceList.setCostPack(0.0);
+            priceList.setItem(item);
+            priceList.setSellingPack(0.0);
+            priceList.setCostPack(0.0);
+            item.setPriceList(priceList);
+            serializables.add(supplier);
+            serializables.add(item);
+            serializables.add(priceList);
+
+            if (manager.update(serializables)) {
+                clear(event);
+                return;
+            }
+            showError("Unable to update " + code);
+        } catch (NumberFormatException e) {
+            showError("Check values again");
+        } catch (Exception e) {
+            Loggings.logError(getClass().getName(), e);
+        }
     }
 
     @FXML
     private void delete(ActionEvent event) {
+        manager.delete(itemTable.getSelectionModel().getSelectedItem().getItem());
+        clear(event);
     }
 
     @FXML
@@ -89,8 +203,9 @@ public class ItemViewController extends SuperController implements Initializable
         descriptionTextField.setText("");
         brandTextField.setText("");
         minimunLimitTextField.setText("");
-
-
+        fillTable();
+        fillSupplierComboBox();
+        fillTypeComboBox();
     }
 
     @FXML
@@ -103,5 +218,106 @@ public class ItemViewController extends SuperController implements Initializable
 
     private void fillTypeComboBox() {
         typeComboBox.setItems(FXCollections.observableList(manager.find(ItemType.class)));
+    }
+
+    private void fillTable() {
+        int i = 0;
+        List<Serializable> serializables = new ArrayList<Serializable>();
+        List<Item> items = manager.find(Item.class);
+        List<ItemTableItem> itemTableItems = new ArrayList<ItemTableItem>();
+        Collections.sort(items);
+        for (Iterator<Item> it = items.iterator(); it.hasNext();) {
+            Item item = it.next();
+            Stock stock = item.getStock();
+            if (stock == null) {
+                stock = new Stock(item.getCode());
+                stock.setBundles(0.0);
+                stock.setItem(item);
+                stock.setQuantity(0.0);
+                item.setStock(stock);
+                serializables.add(stock);
+            }
+            PriceList priceList = item.getPriceList();
+
+            if (priceList == null) {
+                priceList = new PriceList(item.getCode());
+                priceList.setCostPack(0.0);
+                priceList.setCostUnit(0.0);
+                priceList.setSellingPack(0.0);
+                priceList.setSellingUnit(0.0);
+                priceList.setItem(item);
+                item.setPriceList(priceList);
+                serializables.add(priceList);
+            }
+            manager.update(serializables);
+            double paymentAmount = Double.valueOf(priceList.getSellingPack() != null ? priceList.getSellingPack() : 0);
+            itemTableItems.add(new ItemTableItem(++i, item, item.getDescription(), nf3d.format(stock.getQuantity()), nf2d.format(paymentAmount)));
+
+        }
+        itemTable.setItems(FXCollections.observableList(itemTableItems));
+    }
+
+    @FXML
+    private void fill(InputEvent event) {
+        ItemTableItem itemTableItem = itemTable.getSelectionModel().getSelectedItem();
+        if (itemTableItem == null) {
+            return;
+        }
+        fill(itemTableItem.getItem());
+    }
+
+    private void fill(Item item) {
+        if (item == null) {
+            return;
+        }
+        codeTextField.setText(item.getCode());
+        descriptionTextField.setText(item.getDescription());
+        brandTextField.setText(item.getBrand());
+        minimunLimitTextField.setText("" + item.getStock().getMinLimit());
+        supplierComboBox.getSelectionModel().select(item.getSupplier());
+        typeComboBox.getSelectionModel().select(item.getItemTypeType());
+    }
+
+    protected static class ItemTableItem {
+
+        private int row;
+        private String description;
+        private String code;
+        private Item item;
+        private String amount;
+        private String quantity;
+
+        public ItemTableItem(int row, Item item, String description, String quantity, String amount) {
+            this.row = row;
+            this.item = item;
+            this.code = item.getCode();
+            this.description = description;
+            this.amount = amount;
+            this.quantity = quantity;
+        }
+
+        public Item getItem() {
+            return item;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getAmount() {
+            return amount;
+        }
+
+        public String getQuantity() {
+            return quantity;
+        }
     }
 }
