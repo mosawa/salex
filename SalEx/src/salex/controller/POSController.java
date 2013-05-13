7a7946952ae1269922dc7093e6af5d5be070a9cb
anuradha.wijesinghe88 <anuradha.wijesinghe88@gmail.com>
2013-05-13 08:14:07 +0000

    @ExpensesReportView totatlable edit Anchorpane Constraints add fill Button and set fx id
    @pos
    set fx id

--- SalEx/src/salex/controller/POSController.java
+++ SalEx/src/salex/controller/POSController.java
@@ -6,8 +6,15 @@ package salex.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 
 /**
@@ -20,6 +27,54 @@ public class POSController implements Initializable {
     private Font x1;
     @FXML
     private Font x2;
+    @FXML
+    private TextField invoiceNumberTextField;
+    @FXML
+    private HBox dateHBox;
+    @FXML
+    private ComboBox<?> supplierComboBox;
+    @FXML
+    private ComboBox<?> itemComboBox;
+    @FXML
+    private TextField quantityTextField;
+    @FXML
+    private TextField priceTextField;
+    @FXML
+    private TableView<?> posTableView;
+    @FXML
+    private TableColumn<?, ?> hashTableColumn;
+    @FXML
+    private TableColumn<?, ?> codeTableColumn;
+    @FXML
+    private TableColumn<?, ?> descriptionTableColumn;
+    @FXML
+    private TableColumn<?, ?> priceTableColumn;
+    @FXML
+    private TableColumn<?, ?> quantityTableColumn;
+    @FXML
+    private TableColumn<?, ?> netTableColumn;
+    @FXML
+    private TableColumn<?, ?> discountTableColumn;
+    @FXML
+    private TableColumn<?, ?> dTableColumn;
+    @FXML
+    private TableColumn<?, ?> amountTableColumn;
+    @FXML
+    private TextField discountTextField;
+    @FXML
+    private TextField totalAmountTextField;
+    @FXML
+    private TextField paidAmountTextField;
+    @FXML
+    private TextField remainingAmountTextField;
+    @FXML
+    private TextField totalDiscountTextField;
+    @FXML
+    private TextField receiptTextField;
+    @FXML
+    private Button processButton;
+    @FXML
+    private Button clearButton;
 
     /**
      * Initializes the controller class.
@@ -28,4 +83,30 @@ public class POSController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    
+
+    @FXML
+    private void gotoQuantity(ActionEvent event) {
+    }
+
+    @FXML
+    private void gotoPrice(ActionEvent event) {
+    }
+
+    @FXML
+    private void gotoItem(ActionEvent event) {
+    }
+
+    @FXML
+    private void gotoItemComboBox(ActionEvent event) {
+    }
+
+    @FXML
+    private void process(ActionEvent event) {
+    }
+
+    @FXML
+    private void clear(ActionEvent event) {
+    }
 }
