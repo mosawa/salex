3622c891dd7a3efd3c52047bcca0f8ea0a43e88d
anuradha.wijesinghe88@gmail.com <anuradha.wijesinghe88@gmail.com@12557831-1665-e0e3-d81d-a112e9409e98>
2013-05-13 12:09:44 +0000

    i found a error in  PurchaseReportView and I fix it

--- trunk/SalEx/src/salex/controller/PurchaseReportViewController.java
+++ trunk/SalEx/src/salex/controller/PurchaseReportViewController.java
@@ -9,6 +9,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import salex.SuperController;
 
@@ -21,6 +25,18 @@ public class PurchaseReportViewController extends SuperController implements Ini
 
     @FXML
     private HBox dateHbox;
+    @FXML
+    private TableView<?> purchaseReportTableView;
+    @FXML
+    private TableColumn<?, ?> dateTableColumn;
+    @FXML
+    private TableColumn<?, ?> codeTableColumn;
+    @FXML
+    private TableColumn<?, ?> supplierTableColumn;
+    @FXML
+    private TableColumn<?, ?> invoiceTableColumn;
+    @FXML
+    private TableColumn<?, ?> amountTableColumn;
 
     /**
      * Initializes the controller class.
@@ -29,5 +45,13 @@ public class PurchaseReportViewController extends SuperController implements Ini
     public void initialize(URL url, ResourceBundle rb) {
         FXCalendar calendar = new FXCalendar();
         dateHbox.getChildren().add(calendar);
+    }
+
+    @FXML
+    private void fill(MouseEvent event) {
+    }
+
+    @FXML
+    private void fill(KeyEvent event) {
     }
 }
